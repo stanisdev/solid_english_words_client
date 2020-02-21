ce041c9c519d92141fb5f0c890d33c348a8f10e9
Stas <javascript.nodejs.developer@gmail.com>
2020-02-21 11:05:30 +0600

    Developing of app has been started

---
+++ app.js
@@ -0,0 +1,22 @@
+'use string'
+
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const port = 4000;
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.get('/', function(req, res) {
+  res.render('main/index.html');
+});
+
+app.get('/admin/add', function(req, res) {
+  res.render('admin/add.html');
+});
+
+app.listen(port);
+console.log(`Web client was runned on ${port}`);
